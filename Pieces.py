97dd9f80f5cb14bef403f185d6409fee1d0fa812
lukewhite32 <113476319+lukewhite32@users.noreply.github.com>
2022-11-22 14:28:02 -0500

    Update Pieces.py

--- Pieces.py
+++ Pieces.py
@@ -1,6 +1,14 @@
-class Corner:
-  def __init__(self, color1, color2, color3):
+class Corner:                        # Unfortunately, inheritance isn't a thing in python
+  def __init__(self, color1, color2, color3, posX, posY):
     self.color1 = color1
     self.color2 = color2
     self.color3 = color3
-    self.center1 = 
+    self.correctCenter1 = color1
+    self.correctCenter2 = color2
+    self.correctCenter3 = color3
+    self.cubeTwist = 0             # 0 = correct, 1 = twisted clockwise, 2 = twisted counter-clockwise
+    self.posX = posX               # Explained in "cube" class; all in relation to facing white w/blue on top
+    
+ def changePos(self, orient):
+    if orient == "c":
+        if self.posX = 
